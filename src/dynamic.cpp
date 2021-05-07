5eedcd11ce4e45973076ee698a9ddcc8dce108ae
brucechanjianle <jianle001@e.ntu.edu.sg>
2021-05-07 15:40:46 +0800

    feat: add first time param for dyn reconfigure
    This is to resolve ros timer issue.

--- src/dynamic.cpp
+++ src/dynamic.cpp
@@ -7,7 +7,8 @@ dynamic_class::dynamic_class()
     menu_state_(0),
     points_(0.0),
     true_false_state_(true),
-    rate_(5)
+    rate_(5),
+    first_time_(true)
 {
     ;
 }
@@ -21,28 +22,37 @@ dynamic_class::~dynamic_class()
 
 void dynamic_class::start()
 {
-    // Set callback function for dynamic reconfigure (using lambda)
-    server_->setCallback(
-        [this](ros_dynamic_reconfigure::RosDynamicReconfigureConfig & config, uint32_t level)
-        {
-            // Pass settings to local variables
-            this->menu_state_ = config.drop_down_menu;
-            this->true_false_state_ = config.true_or_false;
-            this->points_ = config.slider_points;
-
-            // Display current settings
-            ROS_INFO_STREAM(
-                "\nThe first drop-down: " << config.drop_down_menu <<
-                "\nTrue of false: " << ((config.true_or_false) ? "true" : "false") <<
-                "\nSlider value: " << config.slider_points << std::endl
-            );
-        });
-
     // Load params from server
     private_nh_.param("menu_state", menu_state_, 0);
     private_nh_.param("points", points_, 0.0);
     private_nh_.param("true_false_state", true_false_state_, true);
     private_nh_.param("rate", rate_, 5);
+
+    // Set callback function for dynamic reconfigure (using lambda)
+    server_->setCallback(
+        [this](ros_dynamic_reconfigure::RosDynamicReconfigureConfig & config, uint32_t level)
+        {
+            if(!this->first_time_)
+            {
+                // Pass settings to local variables
+                this->menu_state_ = config.drop_down_menu;
+                this->true_false_state_ = config.true_or_false;
+                this->points_ = config.slider_points;
+
+                // Display current settings
+                ROS_INFO_STREAM(
+                    "\nThe first drop-down: " << config.drop_down_menu <<
+                    "\nTrue of false: " << ((config.true_or_false) ? "true" : "false") <<
+                    "\nSlider value: " << config.slider_points << std::endl
+                );
+            }
+            else
+            {
+                this->first_time_ = false;
+                return;
+            }
+            
+        });
 
     ros::Rate r(rate_);
     while(private_nh_.ok())
